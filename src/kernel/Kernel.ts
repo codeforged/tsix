34751b90c5e989993dd86bfc50d48b3e1dbb79bd
andriansah andri <andriansah@yahoo.com>
2026-09-12 21:14:45 +0700

    change kernel version

--- src/kernel/Kernel.ts
+++ src/kernel/Kernel.ts
@@ -35,7 +35,7 @@ import { std } from "@tsix/Application";
 export class Kernel {
   // Versi kernel saat ini
   private codename: string = "Dinawari";
-  private version: string = "0.2.5.20260831.2";
+  private version: string = "0.2.6.20260912.1";
 
   public getCodename(): string {
     return this.codename;
